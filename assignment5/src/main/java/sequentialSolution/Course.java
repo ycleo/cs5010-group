81813034cf2385f2634df7be0b29f5d1eee3c232
ycleo <leoyclee@gmail.com>
2022-11-11 22:44:39 -0800

    minor methods adjustment
    Former-commit-id: b448dfe94e27286578f25933d58bd06c840b6469

--- assignment5/src/main/java/sequentialSolution/Course.java
+++ assignment5/src/main/java/sequentialSolution/Course.java
@@ -4,6 +4,7 @@ package sequentialSolution;
 import static sequentialSolution.Constants.*;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
 public class Course {
@@ -33,5 +34,34 @@ public class Course {
 
   public TreeMap<Integer, Integer> getDateToSumClicks() {
     return (TreeMap<Integer, Integer>) this.dateToSumClicks;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if(this == o) {
+      return true;
+    }
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
+    }
+    Course course = (Course) o;
+    return Objects.equals(this.codeModule, course.codeModule) ||
+        Objects.equals(this.codePresentation, course.codePresentation) ||
+        Objects.equals(this.courseLength, course.courseLength) ||
+        Objects.equals(this.dateToSumClicks, course.dateToSumClicks);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.codeModule, this.codePresentation, this.courseLength, this.dateToSumClicks);
+  }
+
+  @Override
+  public String toString() {
+    return "Course: { code module: " + this.codeModule + "; "
+        + "code presentation: " + this.codePresentation + "; "
+        + "course length: " + this.courseLength + "; "
+        + "date to sum clicks tree map: " + this.dateToSumClicks.toString()
+        + " }";
   }
 }
--- assignment5/src/main/java/sequentialSolution/SummaryGenerator.java
+++ assignment5/src/main/java/sequentialSolution/SummaryGenerator.java
@@ -10,24 +10,31 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
 
 public class SummaryGenerator {
 
+  private String summaryOutputPath;
+  private HashMap<String, Course> coursesMap;
 
-  public void generateSummary(String summaryOutputPath, HashMap<String, Course> coursesMap)
-      throws IOException {
+  public SummaryGenerator(String summaryOutputPath, HashMap<String, Course> coursesMap) {
+    this.summaryOutputPath = summaryOutputPath;
+    this.coursesMap = coursesMap;
+  }
 
+  public void generateSummary() throws IOException {
 
-    Iterator courseIterator = coursesMap.entrySet().iterator();
+    Iterator courseIterator = this.coursesMap.entrySet().iterator();
     while (courseIterator.hasNext()) {
-
       Map.Entry coursePair = (Map.Entry) courseIterator.next();
       String courseModuleAndPresentation = (String) coursePair.getKey();
       Course course = (Course) coursePair.getValue();
+
       TreeMap<Integer, Integer> dateToSumClicks = course.getDateToSumClicks();
       ArrayList<ArrayList<String>> dateSumClicksArray = mapToArray(dateToSumClicks);
+
       FileWriter summaryWriter = new FileWriter(new File(summaryOutputPath + SLASH + courseModuleAndPresentation + CSV_EXTENSION));
       summaryWriter.append(OUTPUT_ROW_FORMAT + NEXT_LINE);
 
@@ -35,12 +42,11 @@ public class SummaryGenerator {
         summaryWriter.append(String.join(COMMA, rowData));
         summaryWriter.append(NEXT_LINE);
       }
+
       summaryWriter.flush();
       summaryWriter.close();
-
       courseIterator.remove();
     }
-
   }
 
   private ArrayList<ArrayList<String>> mapToArray(TreeMap<Integer, Integer> map) {
@@ -59,5 +65,28 @@ public class SummaryGenerator {
     return arr;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if(this == o) {
+      return true;
+    }
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
+    }
+    SummaryGenerator summaryGenerator = (SummaryGenerator) o;
+    return Objects.equals(this.summaryOutputPath, summaryGenerator.summaryOutputPath) ||
+        Objects.equals(this.coursesMap, summaryGenerator.coursesMap);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.summaryOutputPath, this.coursesMap);
+  }
+
+  @Override
+  public String toString() {
+    return "Summary Generator: { Summary Output Path: " + this.summaryOutputPath + "; "
+       + "Courses Map: " + this.coursesMap.toString() + " }";
+  }
 
 }
