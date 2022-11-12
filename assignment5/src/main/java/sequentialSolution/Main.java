5bb634dc509b2d5aabd36600bc77c58562e5fc01
ycleo <leoyclee@gmail.com>
2022-11-11 23:29:53 -0800

    add illegal argument message
    Former-commit-id: f2c80d4902c5fbdaad157059e10e0cfed21a54e2

--- assignment5/src/main/java/sequentialSolution/Main.java
+++ assignment5/src/main/java/sequentialSolution/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     if (args.length != THREE) {
-      throw new IllegalArgumentException();
+      throw new IllegalArgumentException(ILLEGAL_ARGUMENT_MESSAGE);
     }
     coursesCsvPath = args[ZERO].trim();
     studentVleCsvPath = args[ONE].trim();
