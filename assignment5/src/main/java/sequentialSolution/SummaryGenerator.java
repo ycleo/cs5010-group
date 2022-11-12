b9bcc12f0487502e5a6851868ae30ccf2fa2898a
ycleo <leoyclee@gmail.com>
2022-11-11 23:15:02 -0800

    simplify the tree map iteration
    Former-commit-id: 6d2319c23ec5f635ad1e65811131f0c9a2b390df

--- assignment5/src/main/java/sequentialSolution/SummaryGenerator.java
+++ assignment5/src/main/java/sequentialSolution/SummaryGenerator.java
@@ -25,45 +25,31 @@ public class SummaryGenerator {
   }
 
   public void generateSummary() throws IOException {
-
+    // iterate through courses map
     Iterator courseIterator = this.coursesMap.entrySet().iterator();
     while (courseIterator.hasNext()) {
       Map.Entry coursePair = (Map.Entry) courseIterator.next();
+
       String courseModuleAndPresentation = (String) coursePair.getKey();
       Course course = (Course) coursePair.getValue();
 
-      TreeMap<Integer, Integer> dateToSumClicks = course.getDateToSumClicks();
-      ArrayList<ArrayList<String>> dateSumClicksArray = mapToArray(dateToSumClicks);
-
+      // set up the file writing path and write the format (first row)
       FileWriter summaryWriter = new FileWriter(
           new File(summaryOutputPath + SLASH + courseModuleAndPresentation + CSV_EXTENSION));
       summaryWriter.append(OUTPUT_ROW_FORMAT + NEXT_LINE);
 
-      for (ArrayList<String> rowData : dateSumClicksArray) {
-        summaryWriter.append(String.join(COMMA, rowData));
-        summaryWriter.append(NEXT_LINE);
+      // iterate the information tree map of the current course
+      TreeMap<Integer, Integer> dateToSumClicks = course.getDateToSumClicks();
+      for (Map.Entry<Integer, Integer> entry : dateToSumClicks.entrySet()) {
+        String date = String.valueOf(entry.getKey());
+        String sumClicks = String.valueOf(entry.getValue());
+        summaryWriter.append(date + COMMA + sumClicks + NEXT_LINE);
       }
 
       summaryWriter.flush();
       summaryWriter.close();
       courseIterator.remove();
     }
-  }
-
-  private ArrayList<ArrayList<String>> mapToArray(TreeMap<Integer, Integer> map) {
-    ArrayList<ArrayList<String>> arr = new ArrayList<ArrayList<String>>();
-    Set entries = map.entrySet();
-    Iterator entriesIterator = entries.iterator();
-    while (entriesIterator.hasNext()) {
-      Map.Entry mapping = (Map.Entry) entriesIterator.next();
-      ArrayList<String> item = new ArrayList<>(
-          List.of(
-              String.valueOf(mapping.getKey()),
-              String.valueOf(mapping.getValue()))
-      );
-      arr.add(item);
-    }
-    return arr;
   }
 
   @Override
