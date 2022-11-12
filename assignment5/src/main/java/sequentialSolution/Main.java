9c7f6671058c6e753e54ec7a305577b5d64de6d9
ycleo <leoyclee@gmail.com>
2022-11-11 20:50:31 -0800

    create Main class
    Former-commit-id: 16350f56b482fc359d817b0e08aea29ff595e1de

---
+++ assignment5/src/main/java/sequentialSolution/Main.java
@@ -0,0 +1,28 @@
+package sequentialSolution;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+  static String coursesCsvPath;
+  static String studentVleCsvPath;
+  static String summaryOutputPath;
+  static HashMap<String, Course> coursesMap; // module_presentation -> Course Object
+
+  public static void main(String[] args) throws IOException {
+    coursesCsvPath = args[0].trim();
+    studentVleCsvPath = args[1].trim();
+    summaryOutputPath = args[2].trim();
+    FileReader fileReader = new FileReader();
+    SummaryGenerator summaryGenerator = new SummaryGenerator();
+
+    coursesMap = fileReader.readCoursesCsv(coursesCsvPath); // Read courses.csv and establish courses map
+    fileReader.readStudentVleCsv(studentVleCsvPath, coursesMap); // Sequentially read studentVle.csv and update the courses map
+    // output the summary
+    summaryGenerator.generateSummary(summaryOutputPath, coursesMap);
+  }
+}
