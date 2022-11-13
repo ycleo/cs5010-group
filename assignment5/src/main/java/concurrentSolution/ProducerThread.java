19a6b24aa2dd021efb1a411e88e1d12014f509c7
ycleo <leoyclee@gmail.com>
2022-11-12 21:45:31 -0800

    implement Producer Thread program

--- assignment5/src/main/java/concurrentSolution/ProducerThread.java
+++ assignment5/src/main/java/concurrentSolution/ProducerThread.java
@@ -1,26 +1,55 @@
 package concurrentSolution;
 
+import static concurrentSolution.Driver.progress;
+import static sequentialSolution.Constants.*;
+import static sequentialSolution.FileReader.trimQuotationMark;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.ConcurrentHashMap;
-import sequentialSolution.Course;
+import java.util.concurrent.BlockingDeque;
 
 public class ProducerThread implements Runnable {
 
   private String studentVleCsvPath;
-  private ConcurrentHashMap<String, Course> courseConcurrentHashMap;
+  private BlockingDeque<ArrayList<String>> studentVleBlockingQueue;
   private Scanner csvScanner;
+  private String[] studentVleFormat;
 
-  public ProducerThread(String studentVleCsvPath, ConcurrentHashMap<String, Course> courseConcurrentHashMap) {
+  public ProducerThread(String studentVleCsvPath, BlockingDeque<ArrayList<String>> studentVleBlockingQueue) {
     this.studentVleCsvPath = studentVleCsvPath;
-    this.courseConcurrentHashMap = courseConcurrentHashMap;
+    this.studentVleBlockingQueue = studentVleBlockingQueue;
   }
 
   @Override
   public void run() {
     System.out.println("Starting data producer");
-    while (true) {
-
+    try {
+      this.csvScanner = new Scanner(new File(this.studentVleCsvPath));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
     }
+    this.studentVleFormat = trimQuotationMark(this.csvScanner.nextLine().split(INFO_DELIMITER));
+    while (this.csvScanner.hasNext()) {
+//      System.out.println("Producer is awake");
+      try {
+        String[] studentVle = trimQuotationMark(this.csvScanner.nextLine().split(INFO_DELIMITER));
+
+        // Produce course's date and sum clicks
+        String course = studentVle[ZERO] + UNDERLINE + studentVle[ONE]; // module_presentation
+        String date = studentVle[FOUR];
+        String sumClicks = studentVle[FIVE];
+        ArrayList<String> data = new ArrayList<>(
+            List.of(course, date, sumClicks)
+        );
+
+        this.studentVleBlockingQueue.put(data);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    progress = "read done";
   }
 }
