f5e1c65900ca4a64aee7087ed408ebff6b5cdf65
ycleo <leoyclee@gmail.com>
2022-12-06 17:44:11 -0800

    define message identifiers

---
+++ assignment6/src/main/java/MessageIdentifiers.java
@@ -0,0 +1,11 @@
+public class MessageIdentifiers {
+  public static final int CONNECT_MESSAGE = 19;
+  public static final int CONNECT_RESPONSE = 20;
+  public static final int DISCONNECT_MESSAGE = 21;
+  public static final int QUERY_CONNECTED_USERS = 22;
+  public static final int QUERY_USER_RESPONSE = 23;
+  public static final int BROADCAST_MESSAGE = 24;
+  public static final int DIRECT_MESSAGE = 25;
+  public static final int FAILED_MESSAGE = 26;
+  public static final int SEND_INSULT = 27;
+}
