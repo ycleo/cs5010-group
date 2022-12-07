828430607b9d7893b9e1a7d28c83d3ee0e9916e2
ycleo <leoyclee@gmail.com>
2022-12-07 14:16:57 -0800

    add server javadoc

--- assignment6/src/main/java/Server.java
+++ assignment6/src/main/java/Server.java
@@ -2,19 +2,29 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Server class that accept connection from clients and instantiate corresponding client handlers
+ */
 public class Server {
-
+  final private String connectedMessage = "A new client has connected!";
   private ServerSocket serverSocket;
 
+  /**
+   * Constructs server instance
+   * @param serverSocket server socket
+   */
   public Server(ServerSocket serverSocket) {
     this.serverSocket = serverSocket;
   }
 
+  /**
+   * Starts the server
+   */
   public void startServer() {
     try {
       while (!serverSocket.isClosed()) {
         Socket socket = serverSocket.accept();
-        System.out.println("A new client has connected!");
+        System.out.println(connectedMessage);
         ClientHandler clientHandler = new ClientHandler(socket);
 
         Thread thread = new Thread(clientHandler);
@@ -25,6 +35,9 @@ public class Server {
     }
   }
 
+  /**
+   * Closes the server
+   */
   public void closeServerSocket() {
     try {
       if (serverSocket != null) {
@@ -35,6 +48,11 @@ public class Server {
     }
   }
 
+  /**
+   * Executes the server program
+   * @param args arguments
+   * @throws IOException I/O exception
+   */
   public static void main(String[] args) throws IOException {
     ServerSocket serverSocket = new ServerSocket(Constants.SERVER_PORT);
     Server server = new Server(serverSocket);
